0a38b61dad14b2a8572f8b49c0a7c67f4ec00284
brigremlin <brigrimsley@gmail.com>
2022-08-10 17:03:50 -0500

    bg: added capacitor for mobile launch and updated styling

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'DrinkUp',
+  webDir: 'dist/drink-up',
+  bundledWebRuntime: false
+};
+
+export default config;
